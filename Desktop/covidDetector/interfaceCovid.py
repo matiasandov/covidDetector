23c62c60ed26d4a04cee40fb52d4d328116ab5d8
Matt <a01422885@itesm.mx>
2020-11-07 22:13:22 -0600

    se creo interface streamlit

---
+++ Desktop/covidDetector/interfaceCovid.py
@@ -0,0 +1,13 @@
+# pylint: disable=E1120
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+import plotly.express as px
+
+
+
+st.title("Covid Detector")
+st.markdown("Datos del usuario")
+
+st.header("Datos generales")
